ae67592d145795c49f197370d2d9133e178c00e0
AndreyKirshin <volk-nora@list.ru>
2017-04-26 21:30:53 +0400

    +Batnic

---
+++ Start.bat
@@ -0,0 +1,3 @@
+javac -classpath ./classes -d ./classes src/javase01/t01/logic/Logic.java
+javac -classpath ./classes -d ./classes src/javase01/t01/main/Main.java
+java -classpath ./classes javase01/t01/main/Main
